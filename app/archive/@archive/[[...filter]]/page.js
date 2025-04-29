cd841e3054f6f9fedcccb2f904a802f1c42f3204
anahitaYH <ana.yh021@gmail.com>
2025-04-29 16:02:03 +0330

    Configuring Catch-All Routes

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,26 @@
+import NewsList from "@/components/news-list";
+import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import Link from "next/link";
+
+export default function FilteredNewsPage({params}){
+    const filter = params.filter
+    const links = getAvailableNewsYears()
+    console.log(filter)
+    // const news = getNewsForYear(filter)
+    
+    return(
+    // <NewsList news={news}/>
+    <header id="archive-header">
+            <nav>
+                <ul>
+                    {links.map(link => (
+                        <li key={link}>
+                            <Link href={`/archive/${link}`}>{link}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            
+        </header>
+    )
+}
