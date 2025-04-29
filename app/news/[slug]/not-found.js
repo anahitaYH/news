9639ae5757b88639a99d26d0f8ada7feb1059e27
anahitaYH <ana.yh021@gmail.com>
2025-04-29 13:18:17 +0330

    Handling Not Found Errors & Showing Not Found Pages

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage(){
+    return(
+        <div id="error">
+            <h1>Not Found!</h1>
+            <p>unfortunately, we could not find the requested article.</p>
+        </div>
+    )
+}
