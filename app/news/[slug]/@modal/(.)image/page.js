a4023a958975687b9896c3ba95a1169220ff2692
anahitaYH <ana.yh021@gmail.com>
2025-06-29 15:07:15 +0330

    Navigating Programmatically

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,16 +1,20 @@
+"use client"
+
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({params}){
   const newsItemSlug = params.slug
-    const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemSlug)
-    
+  const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemSlug)
+  const router = useRouter()
+  
     if(!newsItem){
         notFound()
     }
 
     return(
         <>
-            <div className="modal-backdrop" />
+            <div className="modal-backdrop" onClick={router.back}/>
             <dialog className="modal" open>
                 <div className="fullscreen-image">
                     <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
